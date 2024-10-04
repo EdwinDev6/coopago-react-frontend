8fc878017acefc4415a3c19720784e1cdb1170ae
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-04 12:29:43 -0400

    refactor: converting menu responsive

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useRef } from "react";
 import classNames from "classnames";
 import { HiOutlineLogout } from "react-icons/hi";
-import { DASHBOARD_SIDEBAR_LINKS } from "../../libs/constants";
-import { DASHBOARD_SIDEBAR_BOTTOM_LINKS } from "../../libs/constants";
+import {
+  DASHBOARD_SIDEBAR_LINKS,
+  DASHBOARD_SIDEBAR_BOTTOM_LINKS,
+} from "../../libs/constants";
 import { Link, useLocation } from "react-router-dom";
 import logotipo from "../../assets/images/daite.png";
 import { useNavigate } from "react-router-dom";
@@ -12,9 +15,10 @@ import { logoutUrl } from "../../config";
 const linkClass =
   "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
 
-export default function Sidebar() {
+export default function Sidebar({ isOpen, closeSidebar }) {
   const { setAuth } = useAuth();
   const navigate = useNavigate();
+  const sidebarRef = useRef();
 
   const handleLogout = async () => {
     try {
@@ -24,28 +28,53 @@ export default function Sidebar() {
         throw new Error("Error al cerrar sesión");
       }
       setAuth({});
-
       sessionStorage.removeItem("auth");
-
       navigate("/login");
     } catch (error) {
       console.error(error);
     }
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        closeSidebar();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [closeSidebar]);
+
   return (
-    <div className="bg-neutral-900 w-60 p-3 flex flex-col">
+    <div
+      ref={sidebarRef}
+      className={classNames(
+        "bg-neutral-900 w-60 p-3 flex flex-col absolute top-0 left-0 h-full transition-transform transform",
+        {
+          "translate-x-0": isOpen,
+          "-translate-x-full": !isOpen,
+        },
+        "md:static md:translate-x-0"
+      )}
+    >
       <div className="flex items-center gap-2 px-1 py-3">
         <img src={logotipo} alt="Coopago" className="w-10 h-10" />
         <span className="text-neutral-200 text-lg">CooPagos</span>
       </div>
+
       <div className="py-8 flex flex-1 flex-col gap-0.5">
         {DASHBOARD_SIDEBAR_LINKS.map((link) => (
-          <SidebarLink key={link.key} link={link} />
+          <SidebarLink key={link.key} link={link} closeSidebar={closeSidebar} />
         ))}
       </div>
+
       <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
         {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((link) => (
-          <SidebarLink key={link.key} link={link} />
+          <SidebarLink key={link.key} link={link} closeSidebar={closeSidebar} />
         ))}
         <div
           className={classNames(linkClass, "cursor-pointer text-red-500")}
@@ -61,7 +90,7 @@ export default function Sidebar() {
   );
 }
 
-function SidebarLink({ link }) {
+function SidebarLink({ link, closeSidebar }) {
   const { pathname } = useLocation();
 
   return (
@@ -73,9 +102,9 @@ function SidebarLink({ link }) {
           : "text-neutral-400",
         linkClass
       )}
+      onClick={closeSidebar}
     >
       <span className="text-xl">{link.icon}</span>
-
       {link.label}
     </Link>
   );
