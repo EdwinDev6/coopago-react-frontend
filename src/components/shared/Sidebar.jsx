b83cbc710bdf481c5df3d2dccd8316b549126d23
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-11-19 10:18:59 -0500

    feat: add Dropdown component and integrate into Sidebar for improved navigation

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import classNames from "classnames";
 import { HiOutlineLogout } from "react-icons/hi";
 import {
@@ -11,6 +11,14 @@ import logotipo from "../../assets/images/daite.png";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import { logoutUser } from "../../api";
+import Dropdown from "../Dropdown";
+import {
+  MdAccountBalance,
+  MdAccountBalanceWallet,
+  MdPaid,
+} from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+
 const linkClass =
   "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
 
@@ -18,20 +26,24 @@ export default function Sidebar({ isOpen, closeSidebar }) {
   const { setAuth } = useAuth();
   const navigate = useNavigate();
   const sidebarRef = useRef();
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
   const handleLogout = async () => {
-    logoutUser().then(() => {
+    logoutUser()
+      .then(() => {
         setAuth({});
         navigate("/login", { replace: true });
       })
       .catch(() => {
-        navigate("/login")
+        navigate("/login");
       });
   };
-  console.log("Sidebar")
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
         closeSidebar();
+        setActiveDropdown(null);
       }
     };
     document.addEventListener("mousedown", handleClickOutside);
@@ -40,6 +52,10 @@ export default function Sidebar({ isOpen, closeSidebar }) {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [closeSidebar]);
+
+  const toggleDropdown = (label) => {
+    setActiveDropdown((prev) => (prev === label ? null : label));
+  };
 
   return (
     <div
@@ -59,12 +75,59 @@ export default function Sidebar({ isOpen, closeSidebar }) {
           <span className="text-neutral-200 text-lg">CooPagos</span>
         </div>
       </Link>
-    
 
-      <div className="py-8 flex flex-1 flex-col gap-0.5">
+      <div className="py-6 flex flex-1 flex-col gap-2">
         {DASHBOARD_SIDEBAR_LINKS.map((link) => (
           <SidebarLink key={link.key} link={link} closeSidebar={closeSidebar} />
         ))}
+
+        <Dropdown
+          label="Cuentas"
+          icon={<MdAccountBalanceWallet />}
+          options={[
+            { label: "Estado de Cuentas", path: "/estado-cuenta" },
+            {
+              label: "Agregar Cuenta institucion Asociada",
+              path: "/institucion-asociada",
+            },
+          ]}
+          isActive={activeDropdown === "Cuentas"}
+          toggleDropdown={() => toggleDropdown("Cuentas")}
+        />
+        <Dropdown
+          label="Pagos"
+          icon={<MdPaid />}
+          options={[
+            { label: "Pago o Tranferencia", path: "/pagos" },
+            { label: "Pago en Lote", path: "/pago-lote" },
+            { label: "Pago Recurrente", path: "/pago-recurrente" },
+          ]}
+          isActive={activeDropdown === "Pagos"}
+          toggleDropdown={() => toggleDropdown("Pagos")}
+        />
+        <Dropdown
+          label="Beneficiarios"
+          icon={<FaUsers />}
+          options={[
+            { label: "Beneficiarios Interno", path: "/beneficiario-interno" },
+            {
+              label: "Beneficiario Institucion Asociada",
+              path: "/beneficiario-institucion-asociada",
+            },
+          ]}
+          isActive={activeDropdown === "Beneficiarios"}
+          toggleDropdown={() => toggleDropdown("Beneficiarios")}
+        />
+        <Dropdown
+          label="Consultas"
+          icon={<MdAccountBalance />}
+          options={[
+            { label: "Historico de Pagos", path: "/historico-pagos" },
+            { label: "Historico Pagos en Lote", path: "/historico-pagos-lote" },
+          ]}
+          isActive={activeDropdown === "Consultas"}
+          toggleDropdown={() => toggleDropdown("Consultas")}
+        />
       </div>
 
       <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
@@ -94,7 +157,7 @@ function SidebarLink({ link, closeSidebar }) {
       className={classNames(
         pathname === link.path
           ? "bg-neutral-700 text-white"
-          : "text-neutral-400",
+          : "text-neutral-400 bg-neutral-800",
         linkClass
       )}
       onClick={closeSidebar}
