99369e62521dfb474d9f805d42ad7fa1715e42e7
69205208+NomarGomez@users.noreply.github.com <69205208+NomarGomez@users.noreply.github.com>
2024-11-20 09:27:47 -0400

    L

--- src/components/Inputs/SelectInput.jsx
+++ src/components/Inputs/SelectInput.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { getFilters } from "../../api";
 export const SelectInput = React.memo(
-  ({ label, name, value, onChange, filter }) => {
+  ({ label, name, value, selected, onChange, filter }) => {
     const [data, setData] = useState([]);
     React.useEffect(() => {
       getFilters(filter).then((res) => {
@@ -11,14 +11,17 @@ export const SelectInput = React.memo(
     }, []);
     return (
       <div>
-        <label htmlFor={name}>{label}</label>
+        <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 ">{label}</label>
         <select
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           type="text"
           id={name}
           name={name}
           value={value}
           onChange={onChange}
-        ></select>
+        >
+          <option selected>{selected}</option>
+        </select>
       </div>
     );
   }
--- src/components/Inputs/TextInput.jsx
+++ src/components/Inputs/TextInput.jsx
@@ -5,6 +5,7 @@ export const TextInput = React.memo(({ label, name, value, onChange }) => {
     <div>
       <label htmlFor={name}>{label}</label>
       <input
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         type="text"
         id={name}
         name={name}
