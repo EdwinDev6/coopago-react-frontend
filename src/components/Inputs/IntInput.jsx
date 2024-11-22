f2ee8d8964fc61ed220064d9853496fb6177bc18
69205208+NomarGomez@users.noreply.github.com <69205208+NomarGomez@users.noreply.github.com>
2024-11-22 16:31:23 -0400

    Register beneficiary using one form next steps using step form

--- src/components/Inputs/IntInput.jsx
+++ src/components/Inputs/IntInput.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 
-export const TextInput = React.memo(({ label, name, value, onChange, disabled=false, required=false, max=99, placeholder="" }) => {
-  console.log(`Console from ${name}`)
+export const TextInput = React.memo(({ label, name, value, onChange }) => {
   return (
     <div className="mb-2">
       <label className="block mb-1 text-sm font-medium text-gray-900 " htmlFor={name}>{label}</label>
       <input
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        type="text"
+        type="number"
         id={name}
         name={name}
         value={value}
         onChange={onChange}
-        disabled={disabled}
-        required={required}
-        maxLength={max}
-        placeholder={placeholder}
       />
     </div>
   );
--- src/components/Inputs/SelectInput.jsx
+++ src/components/Inputs/SelectInput.jsx
@@ -2,25 +2,30 @@ import React, { useState } from "react";
 import { getFilters } from "../../api";
 export const SelectInput = React.memo(
   ({ label, name, value, selected, onChange, filter }) => {
+    console.log(`Console from ${name}`);
     const [data, setData] = useState([]);
     React.useEffect(() => {
-      getFilters(filter).then((res) => {
-        console.log(res)
-      }).catch((res) => {
-      });
+      getFilters(filter)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((res) => {});
     }, []);
     return (
-      <div>
-        <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 ">{label}</label>
+      <div className="mb-2">
+        <label
+          htmlFor={name}
+          className="block mb-1 text-sm font-medium text-gray-900 "
+        >
+          {label}
+        </label>
         <select
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          type="text"
           id={name}
           name={name}
           value={value}
           onChange={onChange}
         >
-          <option selected>{selected}</option>
         </select>
       </div>
     );
